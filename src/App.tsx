0e173a915581ac30c3618f44a4d90068d95751b2
hkh0105 <hkh9601@naver.com>
2022-12-26 23:47:44 +0900

    Routing, Store, ErrorBoundary 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
-function App() {
+import ErrorBoundary from './components/errorBoundary/ErrorBoundary';
+import Main from './page/main/Main';
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <RecoilRoot>
+          <Routes>
+            <Route path='/' element={<Main />} />
+          </Routes>
+        </RecoilRoot>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
-}
+};
 
 export default App;
